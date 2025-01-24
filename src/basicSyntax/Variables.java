e315eb120a5b4b28ea531fb81885ffb9a299d1f6
yusuke <yusuke@localhost>
2025-01-24 15:28:18 +0900

    変数の実装

---
+++ src/basicSyntax/Variables.java
@@ -0,0 +1,18 @@
+package basicSyntax;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int num1=28;
+		double num2=0.8;
+		String numberLike="私の好きな";
+		String numberDesu="です。";
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		System.out.println(numberLike+"整数は"+num1+numberDesu);
+		System.out.println(numberLike+"少数は"+num2+numberDesu);
+	}
+
+}
