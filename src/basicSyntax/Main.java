ac9bae172448a1e43e53d7eb84d931e99fb10d99
yusuke <yusuke@macbook-air.lan>
2025-02-01 21:01:49 +0900

    クラスの実装

--- src/basicSyntax/Main.java
+++ src/basicSyntax/Main.java
@@ -3,11 +3,9 @@ package basicSyntax;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("こんにちは");
-		System.out.println("私の名前は");
-		System.out.println("Geek君です");
-		
-		System.out.println("こんにちは"+"私の名前は"+"Geek君です");
+		GeekIntroduction.greeting(26,171.4);
+		GeekIntroduction.specialSkill(198,200);
 	}
-
 }
+
+
